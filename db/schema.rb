03d2b8f6e6b906d5450e4e924f80819d774c025c
Tiago Amaro <tiagocis@gmail.com>
2015-12-11 12:31:09 -0200

    following second step of the tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211141110) do
+ActiveRecord::Schema.define(version: 20151211142314) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
